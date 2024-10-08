6acadd346941c9c4c374abab86555130eb92741c
benbasse <moussabasse221@gmail.com>
2024-10-08 18:09:00 +0200

    commit

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -29,8 +29,9 @@ class AuthController extends Controller
         if (!$token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
+        $user = Auth::user();
 
-        return $this->respondWithToken($token);
+        return $this->respondWithToken([$token, $user]);
     }
 
     /**
